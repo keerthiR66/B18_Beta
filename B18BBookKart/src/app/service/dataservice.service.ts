f94be7ff996dd795de60e2dfc10db6db99f8533e
keerthiR66 <97542563+keerthiR66@users.noreply.github.com>
2022-03-12 02:30:35 +0530

    API Registered user Details

---
+++ B18BBookKart/src/app/service/dataservice.service.ts
@@ -0,0 +1,11 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataserviceService {
+  public userDetails = new BehaviorSubject({});
+  constructor() { }
+}
